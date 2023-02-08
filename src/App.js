8a30303bd3dbced2f775acf60df32c3fb23cad48
codnr <wnd75913@gmail.com>
2023-02-08 21:52:15 +0900

    feat: 할 일 목록 제거 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
+import {useState} from "react";
+import styled from "styled-components";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const DummyData = [
+    {id: "1", title: "공부하기", completed: true},
+    {id: "2", title: "청소하기", completed: false},
+  ];
+
+  const [todoData, setTodoData] = useState(DummyData);
+
+  const handleClick = (id) => {
+    setTodoData(todoData.filter((data) => data.id !== id));
+  };
+
+  const BtnStyle = styled.button`
+    border: none;
+    padding: 5px 9px;
+    color: #fff;
+    border-radius: 50px;
+    cursor: pointer;
+    float: right;
+  `;
+  const GetStyle = styled.div`
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+    text-decoration: none;
+  `;
+
   return (
     <div className='container'>
       <div className='todoBlock'>
         <div className='title'>
           <h1>할 일 목록</h1>
         </div>
+        {todoData.map((data) => (
+          <GetStyle key={data.id}>
+            <input type='checkbox' defaultChecked={false} />
+            {data.title}
+            <BtnStyle onClick={() => handleClick(data.id)}>x</BtnStyle>
+          </GetStyle>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
