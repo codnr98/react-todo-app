e327f1184fd9acabab199f3dcd130aea4732e92c
codnr <wnd75913@gmail.com>
2023-02-14 15:08:38 +0900

    style: 모든 style요소 삭제

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,4 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.form`
-  display: flex;
-  margin-top: 5px;
-`;
-
-const TodoInput = styled.input`
-  flex: 10;
-  padding: 5px;
-`;
-
-const TodoBtn = styled.input`
-  flex: 1;
-  padding: 5px 9px;
-  cursor: pointer;
-`;
 
 const Form = ({todoInputValue, setTodoInputValue, todoData, setTodoData}) => {
   const handleChange = (e) => {
@@ -27,19 +10,18 @@ const Form = ({todoInputValue, setTodoInputValue, todoData, setTodoData}) => {
     const newTodo = {id: Date.now(), title: todoInputValue, completed: false};
     setTodoData([...todoData, newTodo]);
     setTodoInputValue("");
-    console.log(todoData);
   };
 
   return (
-    <Container onSubmit={handleSubmit}>
-      <TodoInput
+    <form onSubmit={handleSubmit}>
+      <input
         type='text'
         name='value'
         placeholder='해야 할 일을 입력하세요.'
         value={todoInputValue}
-        onChange={handleChange}></TodoInput>
-      <TodoBtn type='submit' value='입력'></TodoBtn>
-    </Container>
+        onChange={handleChange}></input>
+      <input type='submit' value='입력'></input>
+    </form>
   );
 };
 
