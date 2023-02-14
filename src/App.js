8ac22924005c48d5966b5b64b04b722bb6207911
codnr <wnd75913@gmail.com>
2023-02-14 20:24:56 +0900

    refactor: Lists의 List 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import Form from "./components/Form";
 import "./App.css";
-import List from "./components/List";
+import List from "./components/Lists";
 
 const App = () => {
   const DummyData = [
