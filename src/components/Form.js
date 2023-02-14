2f8eb7b7127dd46d06fad9805884c2ec0534b695
codnr <wnd75913@gmail.com>
2023-02-14 15:48:02 +0900

    style: tillwind css 로 style 변경

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,14 +13,18 @@ const Form = ({todoInputValue, setTodoInputValue, todoData, setTodoData}) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className='flex pt-1' onSubmit={handleSubmit}>
       <input
+        className='w-full px-3 py-2 mr-2 text-gray-500 border rounded'
         type='text'
         name='value'
         placeholder='해야 할 일을 입력하세요.'
         value={todoInputValue}
         onChange={handleChange}></input>
-      <input type='submit' value='입력'></input>
+      <input
+        className='p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200'
+        type='submit'
+        value='입력'></input>
     </form>
   );
 };
